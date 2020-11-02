775e1d6a27a8245fcd7905f66b3a20a08493bd6f
Lucca Nunes <luccanunesgit@gmail.com>
2020-11-02 14:34:57 -0300

    fix typo

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,7 +6,7 @@ const Home = () => (
     <div className="home">
         <img src="https://camo.githubusercontent.com/640b98b021c6efb8c909e62dae5dfc816082e7c2/68747470733a2f2f692e696d6775722e636f6d2f46764b733448722e706e67" alt="" />
         <h1>A Discord API wrapper for making C++ bots</h1>
-        <Link to="/documention/content" className="docs-link">
+        <Link to="/documentation/content" className="docs-link">
             see the docs
         </Link>
     </div>
